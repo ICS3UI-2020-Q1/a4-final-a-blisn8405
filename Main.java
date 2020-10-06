11a5887f895dd9a34a0c136b67c97697a83acd67
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-05 22:55:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner; 
 /**
  *
- * @author 
+ * @author  
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+// create a scanner for user input
+Scanner input = new Scanner(System.in); 
+ //create variable for user input
+   System.out.println("Please enter a positive integer to determine its factors");
+   int number = input.nextInt(); 
+   System.out.println("The factors of " + number +  " are:");   
+
+int count = 1;
+while(number >= count){
+  count = count + 1;
+if(number % count == 0){
+   System.out.println( + count);
+}
+ 
+}
+
+
   }
 }
